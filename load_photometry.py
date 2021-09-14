bb57162bb62f90fc17161ca803fb457ac0f9dc5d
robbassett <rbassett.astro@gmail.com>
2021-09-14 20:14:32 +1000

    lphot

--- load_photometry.py
+++ load_photometry.py
@@ -1,27 +1,42 @@
 import numpy as np
+from flux_converters import *
 
 class Photometry(object):
 
     def __init__(self,input_file,label_file):
         phot = np.loadtxt(input_file,float)
         labs = np.loadtxt(label_file,str)
+        flx_ver = open(label_file,'r')
+        for i in range(2): line=flx_ver.readline()
+        flx_ver = line.split(' ')[-2]
 
-        self.phot = {}
+        self.data = {}
         for i,ID in enumerate(phot[:,0].astype(int)):
-            self.phot[str(ID)] = {}
             for j,filt in enumerate(labs[:,0]):
-                self.phot[str(ID)][filt] = [phot[i,int(labs[j,1])],phot[i,int(labs[j,2])]]
+                if j == 0:
+                    self.data[str(ID)] = {'redshift':0.0,'phot':{}}
+                elif j == 1:
+                    self.data[str(ID)]['redshift'] = phot[i,int(labs[j,1])]
+                else:
+                    in1,in2 = phot[i,int(labs[j,1])],phot[i,int(labs[j,2])]
+                    if flx_ver == 'AB':
+                        tflx = AB_mag(in1)
+                        terr = tflx-AB_mag(in1+in2)
+                    if flx_ver == 'Zfourge':
+                        tflx,terr = ZFOURGE_flx(in1),ZFOURGE_flx(in2)
+                    if flx_ver == 'Jy':
+                        tflx,terr = Jy(in1),Jy(in2)
+                    else:
+                        tflx,terr = in1,in2
+                    self.data[str(ID)]['phot'][filt] = [tflx,terr]
 
-    def ID_2_arrays(self,ID,filters):
+    def get_fit_filters(self,ID,filters):
 
-        tmf = self.phot[str(ID)]
-        wav = np.zeros(len(list(tmf.keys())))
-        flx = np.zeros(len(list(tmf.keys())))
-        err = np.zeros(len(list(tmf.keys())))
+        tmf = self.data[str(ID)]['phot']
+        filters_match = {}
         for i,k in enumerate(tmf.keys()):
-            wav[i] = (np.average(filters[k]['wav'],weights=filters[k]['T']))
-            flx[i] = tmf[k][0]
-            err[i] = tmf[k][1]
+            if tmf[k][0] > 0:
+                filters_match[k] = {'wav':filters[k]['wav'],'T':filters[k]['T']}
 
-        return wav,flx,err
+        return list(filters_match.keys())
         
